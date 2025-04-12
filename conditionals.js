acb1948320fb367f0dc51c8a95f43aaa05e62bdd
Pepper <pandapup3000@gmail.com>
2025-04-11 19:48:54 -0500

    added fluff

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+// Grade Checker
+
+let score = 82;
+// Print A, B, C, D, or F based on score
+// Pizza Quantity Warning
+
+let quantity = 12;
+// Print warning if quantity > 10
+// Login Simulator
+
+let password = "letmein"; // or use prompt
+// Print "Access granted" or "Access denied"
---
+++ functions.js
@@ -0,0 +1,15 @@
+// Dog Age Converter
+
+function dogToHumanYears(dogAge) {
+  
+}
+// Tip Calculator
+
+function calculateTip(total, percentage) {
+  
+}
+//Even or Odd
+
+function checkEvenOdd(num) {
+  
+}

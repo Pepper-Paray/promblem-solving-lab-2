b73457774a7e3ecf853f328415bac7a90cb19753
Pepper <pandapup3000@gmail.com>
2025-04-16 20:36:47 -0500

    added new js about arrays

---
+++ part4-arrays.js
@@ -0,0 +1,29 @@
+//Problem 1: Favorite Foods
+
+let favoritefood = [
+    "chicken and dumplings",
+    "fried chicken",
+    "strawberries",
+    "pancakes",
+    "watermelon"
+];
+console.log(favoritefood);
+favoritefood.push("spagetti");
+
+//Problem 2: Guest List
+let People = [
+    "ashley",
+    "mark",
+    "wilhelmina",
+    "paulette",
+    "thomas",
+    "lois",
+    "clark"
+]
+console.log(People)
+People.shift("ashley")
+console.log(People)
+People.push("ash")
+console.log(People)
+let length = People.length
+console(length)
